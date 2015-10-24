7e162541517aa5ea7574d3978296b02e9f3300f8
thinhlord <thinhnd_57@vnu.edu.vn>
2015-10-24 14:19:48 +0700

    Some new shit 2

---
+++ app/src/main/java/com/teamx/soon/EventDetailFragment.java
@@ -0,0 +1,64 @@
+package com.teamx.soon;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teamx.soon.item.Event;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link EventDetailFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EventDetailFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private Event mParam1;
+
+
+    public EventDetailFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment EventDetailFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EventDetailFragment newInstance(Event param1) {
+        EventDetailFragment fragment = new EventDetailFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = (Event) getArguments().getSerializable(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_detail, container, false);
+
+    }
+
+
+}
--- app/src/main/java/com/teamx/soon/item/Event.java
+++ app/src/main/java/com/teamx/soon/item/Event.java
@@ -1,10 +1,12 @@
 package com.teamx.soon.item;
 
+import java.io.Serializable;
+
 /**
  * Created by ruler_000 on 24/10/2015.
  * Project: Soon
  */
-public class Event {
+public class Event implements Serializable {
     public String name;
     public String image;
     public String des;
